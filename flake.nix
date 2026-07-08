84dc729ae48905f46ac6fddc780bf9534839e427
Georgi Yordanov <g_yordanov@icloud.com>
2026-07-08 22:37:43 +0300

    export different homeworks from the flake

--- flake.nix
+++ flake.nix
@@ -7,6 +7,28 @@
     { self, nixpkgs, ... }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      mkCRT =
+        N: hash:
+        pkgs.stdenv.mkDerivation {
+          pname = "CRT-Homework";
+          version = N;
+
+          src = pkgs.fetchFromGitHub {
+            owner = "georgiyord";
+            repo = "Chaos-Ray-Tracing";
+            inherit hash;
+            rev = if builtins.stringLength N == 1 then "CRT_0${N}" else "CRT_${N}";
+          };
+
+          buildPhase = ''
+            make crt_release
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp build/Release/crt_release $out/bin/CRT-Homework
+          '';
+        };
     in
     {
       devShells.x86_64-linux.default = pkgs.mkShell {
@@ -22,6 +44,12 @@
         inputsFrom = with pkgs; [ ];
 
         shellHook = "";
+      };
+
+      packages.x86_64-linux = {
+        CRT02 = mkCRT "2" "sha256-HjoDE/W6PTA0Sj2ygWb3M3zPboKzvjSojHJ3kFg00cA=";
+        CRT03 = mkCRT "3" "sha256-n9T/bjAXMRd8AaI49Te3VHOWDKOCfVE2Ga4EjDJDPRE=";
+        CRT04 = mkCRT "4" "sha256-2D0F/vCz+XShO/GRsdHuu6fbwkr5l3tM3Kt4DMEZIS4=";
       };
     };
 }
