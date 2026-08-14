75b4faacd6f356bd0093f307a2380a68b3c71315
Georgi Yordanov <g_yordanov@icloud.com>
2026-08-14 15:36:47 +0300

    added rendering threadpool

---
+++ include/RenderEngine/ThreadPool.hpp
@@ -0,0 +1,36 @@
+#ifndef RENDERENGINE_THREADPOOL_HPP
+#define RENDERENGINE_THREADPOOL_HPP
+
+#include <barrier>
+#include <condition_variable>
+#include <cstddef>
+#include <functional>
+#include <mutex>
+#include <thread>
+#include <vector>
+namespace RenderEngine {
+class ThreadPool {
+  using TaskType = std::function<void()>;
+  std::vector<std::thread> threads_;
+  std::vector<TaskType> tasks_;
+  std::mutex tasksMutex_;
+  std::condition_variable tasksConditionVariable_;
+  bool exitFlag_;
+  bool runFlag_;
+  size_t workerCount_;
+  std::barrier<std::function<void()>> round_;
+
+  ThreadPool();
+
+public:
+  static ThreadPool &getInstance();
+
+  void addTask(TaskType task);
+
+  void startAndWait();
+
+  ~ThreadPool();
+};
+} // namespace RenderEngine
+
+#endif // RENDERENGINE_THREADPOOL_HPP
---
+++ src/ThreadPool.cpp
@@ -0,0 +1,82 @@
+#include "RenderEngine/ThreadPool.hpp"
+
+namespace RenderEngine {
+using TaskType = std::function<void()>;
+
+ThreadPool::ThreadPool()
+    : exitFlag_(false), runFlag_(false),
+      workerCount_(std::thread::hardware_concurrency()),
+      round_(static_cast<ptrdiff_t>(workerCount_ + 1), [this]() {
+        std::unique_lock lock(tasksMutex_);
+        runFlag_ = false;
+      }) {
+  threads_.reserve(workerCount_);
+  for (size_t i = 0; i < workerCount_; ++i) {
+    threads_.emplace_back([this]() {
+      while (true) {
+        {
+          std::unique_lock lock(tasksMutex_);
+          tasksConditionVariable_.wait(
+              lock, [this]() { return runFlag_ || exitFlag_; });
+          if (exitFlag_ && tasks_.empty()) {
+            return;
+          }
+        }
+        while (true) {
+          TaskType task;
+          {
+            std::unique_lock lock(tasksMutex_);
+            if (tasks_.empty()) {
+              break;
+            }
+            task = std::move(tasks_.back());
+            tasks_.pop_back();
+          }
+          task();
+        }
+        round_.arrive_and_wait();
+      };
+    });
+  }
+}
+
+ThreadPool &ThreadPool::getInstance() {
+  static ThreadPool singleton_;
+  return singleton_;
+}
+
+void ThreadPool::addTask(TaskType task) {
+  std::unique_lock lock(tasksMutex_);
+  if (runFlag_) {
+    throw std::runtime_error(
+        "Tried to add buckets while rendering. This should not happen!");
+  }
+  tasks_.push_back(task);
+}
+
+void ThreadPool::startAndWait() {
+  {
+    std::unique_lock lock(tasksMutex_);
+    if (runFlag_) {
+      throw std::runtime_error("Tried to start rendering while already under "
+                               "way. This should not happen!");
+    }
+    runFlag_ = true;
+  }
+  tasksConditionVariable_.notify_all();
+  round_.arrive_and_wait();
+}
+
+ThreadPool::~ThreadPool() {
+  {
+    std::unique_lock lock(tasksMutex_);
+    exitFlag_ = true;
+    runFlag_ = true;
+  }
+  tasksConditionVariable_.notify_all();
+
+  for (auto &thread : threads_) {
+    thread.join();
+  }
+}
+} // namespace RenderEngine
